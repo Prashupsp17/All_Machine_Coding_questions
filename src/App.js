a73a8b3726b0fc460d8bf28e4d77133b268cf078
Prashupsp17 <shindeprashant967@gmail.com>
2026-05-10 20:49:37 +0530

    Lazy Loading Added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import './style.css';
-import { useState, useEffect } from 'react';
-import Pagination from './Pagination';
+import { useState, useEffect,Suspense,lazy } from 'react';
+// import Pagination from './Pagination';
 import Coditas from './Coditas';
-import InfiniteScrolling from './InfiniteScrolling';
+// import InfiniteScrolling from './InfiniteScrolling';
 import OtpInput from './OtpInput';
 import AutoComplete from './AutoComplete';
 import TrafficLight from './TrafficLight';
 import ModalPopUp from './ModalPopUp';
 import CountDownTimer from './CountDownTimer';
-import Retry from './Retry';
+// import Retry from './Retry';
 import ImplementationOfUseFetchHook from './ImplementationOfUseFetchHook';
-import UserForm from './UserForm';
-import FileUpload from './FileUpload';
+// import UserForm from './UserForm';
+// import FileUpload from './FileUpload';
 // import Employee from './UseCallback/Employee.js';
-import Employee from './UseCallBack/Employee.js';
-
+// import Employee from './UseCallBack/Employee.js';
+const InfiniteScrolling = lazy(() => import("./InfiniteScrolling"));
+const Pagination = lazy(() => import("./Pagination"));
+const Retry = lazy(() => import("./Retry"));
+const UserForm = lazy(() => import("./UserForm"));
+const FileUpload = lazy(() => import("./FileUpload"));
+const Employee = lazy(() => import("./UseCallBack/Employee.js") )
 export default function App() {
   return (
     <div>
